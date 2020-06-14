44e3321cd781ab09cedbfbeb0d391ec216dcd62e
白滨 <306558437@qq.com>
2020-06-14 23:12:30 +0800

    10-创建炮台选择UI/11—创建炮台的数据类（保存炮台相关的一些数据）/12-监听炮塔选择的事件（保存当前选择的炮台）/13-检测鼠标点击到了哪个Cube上

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BuildManager : MonoBehaviour
+{
+    public TurretData laserTurretData;
+    public TurretData missileTurretData;
+    public TurretData standardTurretData;
+
+    //表示当前选择的炮台（要建造的炮台）
+    public  TurretData selectedTurretData;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            if (EventSystem.current.IsPointerOverGameObject() == false)
+            {
+                //开发炮台的建造
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                bool isCollider = Physics.Raycast(ray, out hit, 1000, LayerMask. GetMask("MapCube"));
+                if (isCollider) 
+                {
+                    GameObject mapCube = hit.collider.gameObject; //得到点击的mapcube
+                }
+            }
+        }
+    }
+
+    public void OnLaserSelected(bool isOn) 
+    {
+        if (isOn) 
+        {
+            selectedTurretData = laserTurretData;
+        }
+    }
+
+    public void OnMissileSelected(bool isOn) 
+    {
+        if (isOn)
+        {
+            selectedTurretData = missileTurretData;
+        }
+    }
+
+    public void OnStandardSelected(bool isOn) 
+    {
+        if (isOn)
+        {
+            selectedTurretData = standardTurretData;
+        }
+    }
+}
