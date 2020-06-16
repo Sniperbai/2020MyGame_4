124f9022bbd76b2243b6860e410238171eadabaf
白滨 <306558437@qq.com>
2020-06-17 06:19:39 +0800

    19-添加触发器，检测炮台附近的敌人

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public List<GameObject> enemys = new List<GameObject>();
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Enemy") 
+        {
+            enemys.Add(col.gameObject);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Enemy") 
+        {
+            enemys.Remove(col.gameObject);
+        }
+    }
+}
