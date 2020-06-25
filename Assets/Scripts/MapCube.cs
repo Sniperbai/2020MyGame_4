b6d812d904233d6762562c2a5068ca6ff72e3d08
白滨 <306558437@qq.com>
2020-06-25 08:33:47 +0800

    28-设置Cube的鼠标移上变红效果

--- Assets/Scripts/MapCube.cs
+++ Assets/Scripts/MapCube.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MapCube : MonoBehaviour
 {
@@ -9,11 +10,30 @@ public class MapCube : MonoBehaviour
 
     public GameObject buildEffect;
 
+    private Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
     public void BuildTurret(GameObject turretPrefab)
     {
         turretGo = GameObject.Instantiate(turretPrefab, transform.position, Quaternion.identity);
         GameObject effect = GameObject.Instantiate(buildEffect, transform.position, Quaternion.identity);
         Destroy(effect, 1);
     }
-    
+
+    void OnMouseEnter()
+    {
+        if (turretGo == null && EventSystem.current.IsPointerOverGameObject()==false) 
+        {
+            renderer.material.color = Color.red;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        renderer.material.color = Color.white; 
+    }
 }
